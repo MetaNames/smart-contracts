b7edce6f13b20130d2c187b78dc7576ab694ea2b
Yeboster <marco@yeboster.com>
2023-08-05 11:14:19 +0200

    refactor(ac): optimize init

--- access-control/src/actions.rs
+++ access-control/src/actions.rs
@@ -9,7 +9,7 @@ pub fn execute_init(msg: ACInitMsg) -> AccessControlState {
     let mut state = AccessControlState {
         roles: SortedVecMap::new(),
     };
-    let role = state.setup_role(DEFAULT_ADMIN_ROLE, &msg.admin_addresses);
+    state.setup_role(DEFAULT_ADMIN_ROLE, &msg.admin_addresses);
 
     state
 }
