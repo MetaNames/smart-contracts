5667a27620df4a3e06472e9d3912d47409d77043
Marco <marco@yeboster.com>
2024-03-12 12:52:17 +0100

    feat(nft): add owner_inventory avl

--- nft/src/tests.rs
+++ nft/src/tests.rs
@@ -35,6 +35,8 @@ fn proper_mint() {
 
     assert_eq!(state.owners.get(&1), Some(mock_address(alice)));
 
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![1]));
+
     assert_eq!(state.token_uri_details.get(&1), Some("token".to_string()));
 }
 
@@ -240,7 +242,9 @@ fn proper_contract_owner_transfer_from() {
         &mut state,
         &transfer_msg,
     );
-    assert_eq!(state.owners.get(&1), Some(mock_address(contract_owner)),);
+    assert_eq!(state.owners.get(&1), Some(mock_address(contract_owner)));
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![]));
+    assert_eq!(state.owners_inventory.get(&mock_address(contract_owner)), Some(vec![1]));
 }
 
 #[test]
@@ -273,6 +277,8 @@ fn proper_owner_transfer_from() {
 
     let _ = execute_transfer_from(&mock_contract_context(alice), &mut state, &transfer_msg);
     assert_eq!(state.owners.get(&1), Some(mock_address(bob)));
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![]));
+    assert_eq!(state.owners_inventory.get(&mock_address(bob)), Some(vec![1]));
 }
 
 #[test]
@@ -313,6 +319,8 @@ fn proper_approved_transfer_from() {
     let _ = execute_transfer_from(&mock_contract_context(bob), &mut state, &transfer_msg);
     assert_eq!(state.owners.get(&1), Some(mock_address(bob)));
     assert_eq!(state.token_approvals.len(), 0);
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![]));
+    assert_eq!(state.owners_inventory.get(&mock_address(bob)), Some(vec![1]));
 }
 
 #[test]
@@ -352,6 +360,8 @@ fn proper_operator_transfer_from() {
 
     let _ = execute_transfer_from(&mock_contract_context(bob), &mut state, &transfer_msg);
     assert_eq!(state.owners.get(&1), Some(mock_address(bob)));
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![]));
+    assert_eq!(state.owners_inventory.get(&mock_address(bob)), Some(vec![1]));
     assert_eq!(state.token_approvals.len(), 0);
     assert_eq!(
         state.operator_approvals.contains_key(&OperatorApproval {
@@ -445,6 +455,7 @@ fn proper_burn() {
     let _ = execute_burn(&mock_contract_context(alice), &mut state, &burn_msg);
     assert_eq!(state.supply, 0);
     assert!(!state.exists(1));
+    assert_eq!(state.owners_inventory.get(&mock_address(alice)), Some(vec![]));
 }
 
 #[test]
